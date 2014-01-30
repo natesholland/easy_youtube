0f32b1ecad517b14f6e8c806b3fbcdd4aa7a70c8
natesholland <natesholland@gmail.com>
2014-01-30 15:03:25 -0800

    adding specs and updating the readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-easy_youtube
+Easy YouTube
 ============
+
+[![Code Climate](https://codeclimate.com/github/natesholland/easy_youtube.png)](https://codeclimate.com/github/natesholland/easy_youtube)
+
+This gem is currently in development and should not be considered stable.
--- spec/easy_youtube_spec.rb
+++ spec/easy_youtube_spec.rb
@@ -5,5 +5,15 @@ describe EasyYouTube do
   	it "should return false for a nil id" do
   		EasyYouTube.valid_id?(nil).should be_false
   	end
+  	it "should return false with an invalid id" do
+  		Net::HTTP.stub(:get).and_return("Invalid id")
+  		EasyYouTube.valid_id?('L57s0Q2qgYg').should be_false
+  		Net::HTTP.stub(:get).and_return("Video not found")
+  		EasyYouTube.valid_id?('L57s0Q2qgYg').should be_false
+  	end
+  	it "should return true with a valid video" do
+  		Net::HTTP.stub(:get).and_return("<?xml version='1.0' encoding='UTF-8'?><entry ... xmlns:media='http://search.yahoo.com/mrss/'")
+  		EasyYouTube.valid_id?('L57s0Q2qgYg').should be_true
+  	end
   end
 end
