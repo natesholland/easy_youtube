5c9df571aca3788d7713c667274b6fcc03ef944f
natesholland <natesholland@gmail.com>
2014-01-31 15:37:12 -0800

    creating more specs and more useful methods

--- lib/easy_youtube.rb
+++ lib/easy_youtube.rb
@@ -1,63 +1,68 @@
 require "net/http"
 class EasyYouTube
-    URL_FORMATS = {
-        :regular => /^(https?:\/\/)?(www\.)?youtube.com\/watch\?(.*\&)?v=(?<id>[^&]+)/,
-        :shortened => /^(https?:\/\/)?(www\.)?youtu.be\/(?<id>[^&]+)/,
-        :embed => /^(https?:\/\/)?(www\.)?youtube.com\/embed\/(?<id>[^&]+)/,
-        :embed_as3 => /^(https?:\/\/)?(www\.)?youtube.com\/v\/(?<id>[^?]+)/,
-        :chromeless_as3 => /^(https?:\/\/)?(www\.)?youtube.com\/apiplayer\?video_id=(?<id>[^&]+)/
-    }
+  URL_FORMATS = {
+    :regular => /^(https?:\/\/)?(www\.)?youtube.com\/watch\?(.*\&)?v=(?<id>[^&]+)/,
+    :shortened => /^(https?:\/\/)?(www\.)?youtu.be\/(?<id>[^&]+)/,
+    :embed => /^(https?:\/\/)?(www\.)?youtube.com\/embed\/(?<id>[^&]+)/,
+    :embed_as3 => /^(https?:\/\/)?(www\.)?youtube.com\/v\/(?<id>[^?]+)/,
+    :chromeless_as3 => /^(https?:\/\/)?(www\.)?youtube.com\/apiplayer\?video_id=(?<id>[^&]+)/
+  }
         
-        INVALID_CHARS = /[^a-zA-Z0-9\:\/\?\=\&\$\-\_\.\+\!\*\'\(\)\,]/
+  INVALID_CHARS = /[^a-zA-Z0-9\:\/\?\=\&\$\-\_\.\+\!\*\'\(\)\,]/
 
-        def self.extract_video_id(youtube_url)
-                return nil if has_invalid_chars?(youtube_url)
-                URL_FORMATS.values.each do |format_regex|
-                        match = format_regex.match(youtube_url)
-                    return match[:id] if match
-                end
-                return nil
-        end
+  def self.extract_video_id(youtube_url)
+    return nil if has_invalid_chars?(youtube_url)
+    URL_FORMATS.values.each do |format_regex|
+      match = format_regex.match(youtube_url)
+      return match[:id] if match
+    end
+    return nil
+  end
 
-        def self.has_invalid_chars?(youtube_url)
-                !INVALID_CHARS.match(youtube_url).nil?
-        end
+  def self.has_invalid_chars?(youtube_url)
+    !INVALID_CHARS.match(youtube_url).nil?
+  end
 
-           def self.youtube_embed_url(youtube_url, width = 420, height = 315)
-            video_id = extract_video_id(youtube_url)
-            %(<iframe width="#{width}" height="#{height}" src="http://www.youtube.com/embed/#{video_id}" frameborder="0" allowfullscreen></iframe>)
-           end
+  def self.youtube_embed_url(youtube_url, width = 420, height = 315)
+    create_embeded_url(extract_video_id(youtube_url), width, height)
+  end
 
-           def self.youtube_regular_url(youtube_url)
-            video_id = extract_video_id(youtube_url)
-            "http://www.youtube.com/watch?v=#{ video_id }"
-          end
+  def self.create_embeded_url(video_id, width = 420, height = 315)
+    %(<iframe width="#{width}" height="#{height}" src="http://www.youtube.com/embed/#{video_id}" frameborder="0" allowfullscreen></iframe>)
+  end
 
-          def self.youtube_shortened_url(youtube_url)
-            video_id = extract_video_id(youtube_url)
-            "http://youtu.be/#{ video_id }"
-          end
+  def self.youtube_regular_url(youtube_url)
+    create_regular_url(extract_video_id(youtube_url))
+  end
+
+  def self.create_regular_url(video_id)
+    "http://www.youtube.com/watch?v=#{video_id}"
+  end
+
+  def self.youtube_shortened_url(youtube_url)
+    video_id = extract_video_id(youtube_url)
+    "http://youtu.be/#{ video_id }"
+  end
         
-        def self.valid_id?(id)
-                if id
-                    response = Net::HTTP.get("gdata.youtube.com", "/feeds/api/videos/#{id}")
-                    if ["Invalid id", "Video not found"].include? response
-                              false
-                    else
-                              true
-                    end
-                else
-                        false
-                end
-        end        
+  def self.valid_id?(id)
+    if id
+      response = Net::HTTP.get("gdata.youtube.com", "/feeds/api/videos/#{id}")
+      if ["Invalid id", "Video not found"].include? response
+        false
+      else
+        true
+      end
+    else
+      false
+    end
+  end        
 
-          def self.valid_youtube_link?(youtube_url)
-                  if has_invalid_chars?(youtube_url)
-                          false
-                  else
-                    video_id = extract_video_id(youtube_url)
-                          valid_id?(id)
-                  end
-          end
-
+  def self.valid_youtube_link?(youtube_url)
+    if has_invalid_chars?(youtube_url)
+      false
+    else
+      video_id = extract_video_id(youtube_url)
+      valid_id?(id)
+    end
+  end
 end
--- spec/easy_youtube_spec.rb
+++ spec/easy_youtube_spec.rb
@@ -22,6 +22,27 @@ describe EasyYouTube do
       EasyYouTube.extract_video_id("not a valid url").should be_nil
       EasyYouTube.extract_video_id("github.com/?id=L57s0Q2qgYg").should be_nil
     end
+    it "should return the proper id from the basic url" do
+      EasyYouTube.extract_video_id("http://www.youtube.com/watch?v=L57s0Q2qgYg").should == "L57s0Q2qgYg"
+      EasyYouTube.extract_video_id("https://www.youtube.com/watch?v=L57s0Q2qgYg").should == "L57s0Q2qgYg"
+      EasyYouTube.extract_video_id("www.youtube.com/watch?v=L57s0Q2qgYg").should == "L57s0Q2qgYg"
+      EasyYouTube.extract_video_id("http://www.youtube.com/watch?feature=player_embedded&v=L57s0Q2qgYg").should == "L57s0Q2qgYg"
+    end
+  end
 
+  describe "the youtube_embeded_url method" do
+    it "should construct valid embeded urls" do
+      EasyYouTube.youtube_embed_url("www.youtube.com/watch?v=L57s0Q2qgYg").should == 
+      %(<iframe width="420" height="315" src="http://www.youtube.com/embed/L57s0Q2qgYg" frameborder="0" allowfullscreen></iframe>)
+
+      EasyYouTube.youtube_embed_url("www.youtube.com/watch?v=L57s0Q2qgYg", 720, 420).should == 
+      %(<iframe width="720" height="420" src="http://www.youtube.com/embed/L57s0Q2qgYg" frameborder="0" allowfullscreen></iframe>)
+    end
+  end
+
+  describe "the youtube_regular_url method" do
+    it "should construct valid urls" do
+      EasyYouTube.youtube_regular_url("http://www.youtube.com/watch?v=L57s0Q2qgYg").should == "http://www.youtube.com/watch?v=L57s0Q2qgYg"
+    end
   end
 end
